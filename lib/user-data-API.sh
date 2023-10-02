4b4a49dc0b4114ac3f690c297cb93c5ad4aaf2ff
Diarmuid O Connor <doconnor@wit.ie>
2023-10-02 12:46:42 +0100

    Fix Mongo access issue

--- lib/user-data-API.sh
+++ lib/user-data-API.sh
@@ -14,5 +14,5 @@ npm run build
 export SEED_DB=true
 export SECRET=ilikecake
 export PORT=3000
-export MONGO_DB='mongodb+srv://readuser:687flM1WOz@doconnor-atlas-cluster.vddmf.mongodb.net/test?retryWrites=true&w=majority'
+export MONGO_DB='mongodb+srv://readwriteuser:q1w2e3r4@doconnor-atlas-cluster.vddmf.mongodb.net/test?retryWrites=true&w=majority'
 npm run start:prod
